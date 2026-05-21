86f46405c60a0f30fc2bfb9004cbb4cdd3223546
Triumph <triumphaidenojie08@gmail.com>
2026-05-21 19:03:25 +0100

    feat: add custom metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,12 @@ import { Header } from "@/components/sections/header";
 import { HeroSection } from "@/components/sections/hero";
 import { StackMarqueeSection } from "@/components/sections/stack-marquee";
 import { WorkSection } from "@/components/sections/work";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Triumph Aidenojie",
+  description: "Teen web and app developer. This is my portfolio website.",
+};
 
 export default function Page() {
   return (
